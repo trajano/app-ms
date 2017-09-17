c22c070e87c640c69e48bad91bcd88da4124046b
Archimedes Trajano <developer@trajano.net>
2017-09-16 22:52:46 -0400

    Simple switch

--- sample-gw/src/main/java/net/trajano/ms/examplegw/Gateway.java
+++ sample-gw/src/main/java/net/trajano/ms/examplegw/Gateway.java
@@ -5,6 +5,7 @@ import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpServerResponse;
 
 public class Gateway extends AbstractVerticle {
 
@@ -18,26 +19,37 @@ public class Gateway extends AbstractVerticle {
     public void start() throws Exception {
 
         final HttpClient client = vertx.createHttpClient(new HttpClientOptions());
-        vertx.createHttpServer().requestHandler(req -> {
-            System.out.println("Proxying request: " + req.uri());
-            final HttpClientRequest c_req = client.request(req.method(), 8080, "localhost", req.uri(), c_res -> {
-                System.out.println("Proxying response: " + c_res.statusCode());
-                req.response().setChunked(true);
-                req.response().setStatusCode(c_res.statusCode());
-                req.response().headers().setAll(c_res.headers());
-                c_res.handler(data -> {
-                    System.out.println("Proxying response body: " + data.toString("ISO-8859-1"));
-                    req.response().write(data);
-                });
-                c_res.endHandler((v) -> req.response().end());
-            });
-            c_req.setChunked(true);
-            c_req.headers().setAll(req.headers());
-            req.handler(data -> {
-                System.out.println("Proxying request body " + data.toString("ISO-8859-1"));
-                c_req.write(data);
-            });
-            req.endHandler((v) -> c_req.end());
-        }).listen(8180);
+        vertx.createHttpServer()
+            .requestHandler(req -> {
+
+                if (req.uri().equals("/hello")) {
+                    // This handler gets called for each request that arrives on the server
+                    final HttpServerResponse response = req.response();
+                    response.putHeader("content-type", "text/plain");
+
+                    // Write to the response and end it
+                    response.end("Hello World!");
+                } else {
+                    System.out.println("Proxying request: " + req.uri());
+                    final HttpClientRequest c_req = client.request(req.method(), 8080, "localhost", req.uri(), c_res -> {
+                        System.out.println("Proxying response: " + c_res.statusCode());
+                        req.response().setChunked(true);
+                        req.response().setStatusCode(c_res.statusCode());
+                        req.response().headers().setAll(c_res.headers());
+                        c_res.handler(data -> {
+                            System.out.println("Proxying response body: " + data.toString("ISO-8859-1"));
+                            req.response().write(data);
+                        });
+                        c_res.endHandler((v) -> req.response().end());
+                    });
+                    c_req.setChunked(true);
+                    c_req.headers().setAll(req.headers());
+                    req.handler(data -> {
+                        System.out.println("Proxying request body " + data.toString("ISO-8859-1"));
+                        c_req.write(data);
+                    });
+                    req.endHandler((v) -> c_req.end());
+                }
+            }).listen(8180);
     }
 }
