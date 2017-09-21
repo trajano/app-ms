1cc517bf341fad5621096733b4d073947fb5b52c
Archimedes Trajano <developer@trajano.net>
2017-09-20 22:48:28 -0400

    Convert main to a verticle

--- ms-engine/src/main/java/net/trajano/ms/engine/sample/Main.java
+++ ms-engine/src/main/java/net/trajano/ms/engine/sample/Main.java
@@ -1,27 +1,34 @@
 package net.trajano.ms.engine.sample;
 
+import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
 import net.trajano.ms.engine.JaxRsRoute;
 
-public class Main {
+public class Main extends AbstractVerticle {
 
     public static void main(final String[] args) {
 
         final VertxOptions options = new VertxOptions();
         Vertx.clusteredVertx(options, event -> {
             final Vertx vertx = event.result();
+            vertx.deployVerticle(new Main());
 
-            final Router router = Router.router(vertx);
-            final HttpServer http = vertx.createHttpServer();
-
-            JaxRsRoute.route(vertx, router, MyApp.class);
-            http.requestHandler(req -> router.accept(req)).listen(8280);
         });
 
         //final Vertx vertx = Vertx.vertx();
 
+    }
+
+    @Override
+    public void start() throws Exception {
+
+        final Router router = Router.router(vertx);
+        final HttpServer http = vertx.createHttpServer();
+
+        JaxRsRoute.route(vertx, router, MyApp.class);
+        http.requestHandler(req -> router.accept(req)).listen(8280);
     }
 }
