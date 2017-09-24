e8cd0a538e910b0463ddf38a89fb6ddc3d53fe28
Archimedes Trajano <developer@trajano.net>
2017-09-24 02:08:47 -0400

    Added SSL support

--- ms-engine/src/main/java/net/trajano/ms/engine/JaxRsVerticle.java
+++ ms-engine/src/main/java/net/trajano/ms/engine/JaxRsVerticle.java
@@ -2,8 +2,12 @@ package net.trajano.ms.engine;
 
 import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.net.PemKeyCertOptions;
 import io.vertx.ext.web.Router;
+import net.trajano.ms.engine.internal.BufferUtil;
 import net.trajano.ms.engine.sample.MyApp;
 import net.trajano.ms.engine.second.MyApp2;
 
@@ -21,7 +25,14 @@ public class JaxRsVerticle extends AbstractVerticle {
         final ConfigRetriever retriever = ConfigRetriever.create(vertx);
 
         final Router router = Router.router(vertx);
-        final HttpServer http = vertx.createHttpServer();
+        final Buffer cert = BufferUtil.bufferFromClasspathResource("cert.pem");
+        final Buffer key = BufferUtil.bufferFromClasspathResource("key.pem");
+        final HttpServerOptions httpServerOptions = new HttpServerOptions()
+            .setUseAlpn(true)
+            .setSsl(true)
+            .setKeyCertOptions(new PemKeyCertOptions().addCertValue(cert).addKeyValue(key));
+
+        final HttpServer http = vertx.createHttpServer(httpServerOptions);
 
         final Integer port = 8280;// config().getJsonObject("http").getInteger("port", 8280);
 
