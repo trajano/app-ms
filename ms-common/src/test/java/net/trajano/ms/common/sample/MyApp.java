4a72e32b0a80e2dbc0827020f7a6fea5c4c935b6
Archimedes Trajano <developer@trajano.net>
2017-10-04 00:00:15 -0400

    Autoreload of vertx config sample

--- ms-common/src/test/java/net/trajano/ms/common/sample/MyApp.java
+++ ms-common/src/test/java/net/trajano/ms/common/sample/MyApp.java
@@ -9,9 +9,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.swagger.annotations.Info;
 import io.swagger.annotations.SwaggerDefinition;
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import net.trajano.ms.common.MicroserviceVerticle;
 
@@ -34,6 +38,11 @@ public class MyApp extends Application {
 
     public static void main(final String[] args) throws Exception {
 
+        run(Vertx.vertx(), MyApp.class);
+    }
+
+    public static void main2(final String[] args) throws Exception {
+
         final VertxOptions vertOptions = new VertxOptions();
         vertOptions.setWarningExceptionTime(1);
         vertOptions.setWorkerPoolSize(50);
@@ -44,6 +53,37 @@ public class MyApp extends Application {
             .put("application_class", MyApp.class.getName())
             .put("http", new JsonObject().put("port", 8900)));
         vertx.deployVerticle(new MicroserviceVerticle(), options);
+    }
+
+    public static void run(final Vertx vertx,
+        final Class<? extends Application> applicationClass) {
+
+        final ConfigStoreOptions yamlStore = new ConfigStoreOptions()
+            .setType("directory")
+            .setConfig(new JsonObject()
+                .put("path", ".")
+                .put("filesets", new JsonArray()
+                    .add(new JsonObject()
+                        .put("pattern", "application.yml")
+                        .put("format", "yaml"))));
+
+        final ConfigRetrieverOptions configRetrieverOptions = new ConfigRetrieverOptions()
+            .addStore(yamlStore);
+        final ConfigRetriever configRetriever = ConfigRetriever.create(vertx, configRetrieverOptions);
+
+        configRetriever.getConfig(r -> {
+            if (r.succeeded()) {
+                System.out.println("HERE");
+                System.out.println(r.result());
+            }
+
+        });
+        configRetriever.listen(event -> {
+
+            System.out.println(vertx.deploymentIDs());
+            System.out.println(event.getNewConfiguration());
+        });
+
     }
 
 }
