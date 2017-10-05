ee30d67a41a744a7650652f6623e1c6b143e834e
Archimedes Trajano <developer@trajano.net>
2017-10-05 19:43:17 -0400

    Fixed proxy configuration

--- ms-common/src/main/java/net/trajano/ms/common/internal/config/ConfigurationProvider.java
+++ ms-common/src/main/java/net/trajano/ms/common/internal/config/ConfigurationProvider.java
@@ -8,12 +8,31 @@ import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.net.ProxyOptions;
+import io.vertx.core.net.ProxyType;
 
 @Configuration
 public class ConfigurationProvider {
 
-    @Value("${http.client.proxy:}")
-    private String httpClientProxy;
+    @Value("${http.client.proxy.host:}")
+    private String httpClientProxyHost;
+
+    @Value("${http.client.proxy.password:}")
+    private String httpClientProxyPassword;
+
+    /**
+     * Uses the default value from {@link ProxyOptions#DEFAULT_PORT}
+     */
+    @Value("${http.client.proxy.port:3128}")
+    private int httpClientProxyPort;
+
+    /**
+     * Uses the default value from {@link ProxyOptions#DEFAULT_TYPE}
+     */
+    @Value("${http.client.proxy.proxyType:HTTP}")
+    private ProxyType httpClientProxyType;
+
+    @Value("${http.client.proxy.username:}")
+    private String httpClientProxyUsername;
 
     @Value("${http.port:8900}")
     private int httpPort;
@@ -28,9 +47,15 @@ public class ConfigurationProvider {
     public HttpClientOptions httpClientOptions() {
 
         final HttpClientOptions options = new HttpClientOptions();
-        final ProxyOptions proxyOptions = new ProxyOptions();
-        proxyOptions.setUsername(httpClientProxyUsername);
-        options.setProxyOptions(proxyOptions);
+        if (httpClientProxyHost != null) {
+            final ProxyOptions proxyOptions = new ProxyOptions()
+                .setHost(httpClientProxyHost)
+                .setPort(httpClientProxyPort)
+                .setType(httpClientProxyType)
+                .setUsername(httpClientProxyUsername)
+                .setPassword(httpClientProxyPassword);
+            options.setProxyOptions(proxyOptions);
+        }
         return options;
     }
 
