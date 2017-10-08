b63e3792dc0af679c6cdd0c036ffc65942210765
Archimedes Trajano <developer@trajano.net>
2017-10-08 13:22:31 -0400

    Added authz stubs

---
+++ sample-authz/src/main/java/net/trajano/ms/example/CacheProvider.java
@@ -0,0 +1,26 @@
+package net.trajano.ms.example;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import net.trajano.ms.common.beans.CommonMs;
+
+@Configuration
+public class CacheProvider {
+
+    @Bean
+    public CacheManager cacheManager() {
+
+        final ConcurrentMapCacheManager concurrentMapCacheManager = new ConcurrentMapCacheManager(CommonMs.JWKS_CACHE);
+        return concurrentMapCacheManager;
+    }
+
+    @Bean(name = CommonMs.JWKS_CACHE)
+    public Cache jwsCache(final CacheManager cacheManager) {
+
+        return cacheManager.getCache(CommonMs.JWKS_CACHE);
+    }
+}
