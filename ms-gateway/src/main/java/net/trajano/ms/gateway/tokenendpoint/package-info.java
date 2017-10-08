9792ae40d6abbc59e46537490b23bb694f2d814b
Archimedes Trajano <developer@trajano.net>
2017-10-07 21:10:39 -0400

    Added new package

---
+++ ms-gateway/src/main/java/net/trajano/ms/gateway/tokenendpoint/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides the core components used for building a token endpoint that would
+ * work with the API gateway.
+ *
+ * @author Archimedes Trajano
+ */
+package net.trajano.ms.gateway.tokenendpoint;
