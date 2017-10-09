58435955c6a9481975819de0a62431d750a3e1b5
Archimedes Trajano <developer@trajano.net>
2017-10-09 16:07:45 -0400

    reverted back to simple

--- sample-gw/src/main/java/net/trajano/ms/examplegw/SampleGateway.java
+++ sample-gw/src/main/java/net/trajano/ms/examplegw/SampleGateway.java
@@ -1,40 +1,12 @@
 package net.trajano.ms.examplegw;
 
-import javax.annotation.PostConstruct;
+import net.trajano.ms.gateway.Gateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner.Mode;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import io.vertx.core.Vertx;
-import net.trajano.ms.gateway.GatewayVerticle;
-
-@SpringBootApplication
-@EnableAutoConfiguration
 public class SampleGateway {
 
     public static void main(final String[] args) {
 
-        System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory");
-        final SpringApplication application = new SpringApplication(SampleGateway.class);
-        application.setBannerMode(Mode.OFF);
-        application.setWebEnvironment(false);
-        application.run(args);
-
-    }
-
-    @Autowired
-    private GatewayVerticle gatewayVerticle;
-
-    @Autowired
-    private Vertx vertx;
-
-    @PostConstruct
-    public void start() {
-
-        vertx.deployVerticle(gatewayVerticle);
+        Gateway.main(args);
     }
 
 }
