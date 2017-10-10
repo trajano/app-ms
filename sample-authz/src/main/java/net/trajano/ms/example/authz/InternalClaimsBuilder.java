a582d681a10595d8ab89a17a3b5032cbe9987748
Archimedes Trajano <developer@trajano.net>
2017-10-10 00:09:05 -0400

    Moved javadoc configuration to the java projects

--- sample-authz/src/main/java/net/trajano/ms/example/authz/InternalClaimsBuilder.java
+++ sample-authz/src/main/java/net/trajano/ms/example/authz/InternalClaimsBuilder.java
@@ -5,14 +5,14 @@ import com.nimbusds.jwt.JWTClaimsSet;
 public interface InternalClaimsBuilder {
 
     /**
-     * This is an extension point that allows the assembly of an internal JWT Claims
-     * set based on data from another JWT Claims Set. It must return a builder
-     * rather than a final one as additional framework level claims are required. At
-     * minimum the subject needs to be set and optionally the "roles" claim can be
-     * populated as well.
+     * This is an extension point that allows the assembly of an internal JWT
+     * Claims set based on data from another JWT Claims Set. It must return a
+     * builder rather than a final one as additional framework level claims are
+     * required. At minimum the subject needs to be set and optionally the
+     * "roles" claim can be populated as well.
      * <p>
-     * The implementation can use the claims to look up from another data store to
-     * get the internal claims used by the application.
+     * The implementation can use the claims to look up from another data store
+     * to get the internal claims used by the application.
      *
      * @param claims
      *            claims
