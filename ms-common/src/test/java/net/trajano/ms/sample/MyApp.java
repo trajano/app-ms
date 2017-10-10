a2b31517547333473edda790c30a86e56b6d0ce2
Archimedes Trajano <developer@trajano.net>
2017-10-09 23:49:33 -0400

    Removed the use of component scan

--- ms-common/src/test/java/net/trajano/ms/sample/MyApp.java
+++ ms-common/src/test/java/net/trajano/ms/sample/MyApp.java
@@ -4,8 +4,7 @@ import javax.annotation.PostConstruct;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.annotations.Info;
 import io.swagger.annotations.SwaggerDefinition;
@@ -15,7 +14,7 @@ import net.trajano.ms.common.Microservice;
  * This is the Jax-RS Application defintion. This is also where Spring
  * annotations can be configured. This will automatically scan for JAX-RS
  * resources in the package of the application. If you want to use Spring
- * components then {@link ComponentScan} should be set.
+ * components then {@link @SpringBootApplication} should be set.
  *
  * @author Archimedes Trajano
  */
@@ -23,8 +22,7 @@ import net.trajano.ms.common.Microservice;
 @SwaggerDefinition(
     info = @Info(title = "Test",
         version = "1.0"))
-@Configuration
-@ComponentScan
+@SpringBootApplication
 public class MyApp extends Application {
 
     public static void main(final String[] args) {
--- sample-ms/src/main/java/net/trajano/ms/example/SampleMain.java
+++ sample-ms/src/main/java/net/trajano/ms/example/SampleMain.java
@@ -3,14 +3,12 @@ package net.trajano.ms.example;
 import javax.ws.rs.core.Application;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import net.trajano.ms.common.Microservice;
 
 @SpringBootApplication
 @EnableScheduling
-@ComponentScan
 public class SampleMain extends Application {
 
     public static void main(final String[] args) {
