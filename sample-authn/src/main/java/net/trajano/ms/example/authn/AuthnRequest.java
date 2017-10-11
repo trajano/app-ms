043639f258c5fd83f531827e836f1db8d98d5062
Archimedes Trajano <arch@trajano.net>
2017-10-11 16:10:23 -0400

    Added no xml access type

--- sample-authn/src/main/java/net/trajano/ms/example/authn/AuthnRequest.java
+++ sample-authn/src/main/java/net/trajano/ms/example/authn/AuthnRequest.java
@@ -1,11 +1,14 @@
 package net.trajano.ms.example.authn;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthnRequest {
 
