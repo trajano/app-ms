7b1b286c4daadc6661b29b3620688a8480b932ce
Archimedes Trajano <developer@trajano.net>
2017-10-20 00:27:27 -0400

    Removed vert.x related code
    This will remove the need to put Vert.X as part of common.

--- ms-swagger/src/main/java/net/trajano/ms/swagger/internal/ClonableSwagger.java
+++ ms-swagger/src/main/java/net/trajano/ms/swagger/internal/ClonableSwagger.java
@@ -1,35 +1,42 @@
 package net.trajano.ms.swagger.internal;
 
+import java.net.URI;
 import java.util.Collections;
+
+import javax.ws.rs.core.UriInfo;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import io.swagger.models.Scheme;
 import io.swagger.models.Swagger;
-import io.vertx.ext.web.RoutingContext;
 
 @JsonInclude(Include.NON_NULL)
 public class ClonableSwagger extends Swagger {
 
     /**
      * Creates a shallow copy of the swagger but set the schemes, host and base
-     * path based on {@link RoutingContext}.
+     * path based on {@link UriInfo}.
      *
-     * @param baseUri
-     *            base URI
+     * @param uriInfo
+     *            URI info of the request.
      * @return shallow copy
      */
-    public ClonableSwagger withRoutingContext(final RoutingContext context) {
+    public ClonableSwagger withUriInfo(final UriInfo uriInfo) {
 
         final ClonableSwagger copy = new ClonableSwagger();
         copy.swagger = swagger;
         copy.info = info;
-        copy.host = context.request().host();
+        final URI requestUri = uriInfo.getRequestUri();
+        if (requestUri.getPort() != -1) {
+            copy.host = requestUri.getHost() + ":" + requestUri.getPort();
+        } else {
+            copy.host = requestUri.getHost();
+        }
         copy.basePath = basePath;
         copy.tags = tags;
 
-        copy.schemes = Collections.singletonList(Scheme.forValue(context.request().scheme()));
+        copy.schemes = Collections.singletonList(Scheme.forValue(requestUri.getScheme()));
 
         copy.consumes = consumes;
         copy.produces = produces;
