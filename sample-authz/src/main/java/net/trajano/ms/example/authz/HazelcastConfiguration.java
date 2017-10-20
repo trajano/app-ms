ee7c3a341cb74c8c70e10d02fb63cfa20b6b1e0e
Archimedes Trajano <developer@trajano.net>
2017-10-19 21:25:11 -0400

    Added Hazelcast event logging
    Fixes #13

--- sample-authz/src/main/java/net/trajano/ms/example/authz/HazelcastConfiguration.java
+++ sample-authz/src/main/java/net/trajano/ms/example/authz/HazelcastConfiguration.java
@@ -1,18 +1,26 @@
 package net.trajano.ms.example.authz;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.config.MapConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.EntryListenerConfig;
+import com.hazelcast.config.MapConfig;
+
 @Configuration
 public class HazelcastConfiguration {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HazelcastConfiguration.class);
+
     @Value("${token.accessTokenExpiration:300}")
     private int accessTokenExpirationInSeconds;
+
+    @Autowired
+    private LoggingEntryListener entryListener;
 
     @Value("${cache.instanceName:authz}")
     private String instanceName;
@@ -20,35 +28,35 @@ public class HazelcastConfiguration {
     @Value("${token.refreshTokenExpiration:3600}")
     private int refreshTokenExpirationInSeconds;
 
-    private static final Logger LOG = LoggerFactory.getLogger(HazelcastConfiguration.class);
-    //
-    //    @Bean
-    //    public HazelcastInstance cacheManager(Config config) {
-    //        return Hazelcast.newHazelcastInstance(config);
-    //    }
-
     @Bean
     public Config hazelcastConfig() {
 
-        Config config = new Config()
+        final EntryListenerConfig listener = new EntryListenerConfig();
+        if (LOG.isDebugEnabled()) {
+            listener.setLocal(true)
+                .setImplementation(entryListener);
+        }
+        final Config config = new Config()
             .setInstanceName(instanceName)
             .setProperty("hazelcast.logging.type", "slf4j")
             .addMapConfig(new MapConfig()
                 .setName(TokenCache.ACCESS_TOKEN_TO_CLAIMS)
                 .setTimeToLiveSeconds(accessTokenExpirationInSeconds)
-                .setMaxIdleSeconds(accessTokenExpirationInSeconds))
+                .setMaxIdleSeconds(accessTokenExpirationInSeconds)
+                .addEntryListenerConfig(listener))
             .addMapConfig(new MapConfig()
                 .setName(TokenCache.REFRESH_TOKEN_TO_ACCESS_TOKEN)
                 .setTimeToLiveSeconds(refreshTokenExpirationInSeconds)
-                .setMaxIdleSeconds(refreshTokenExpirationInSeconds))
+                .setMaxIdleSeconds(refreshTokenExpirationInSeconds)
+                .addEntryListenerConfig(listener))
             .addMapConfig(new MapConfig()
                 .setName(TokenCache.REFRESH_TOKEN_TO_CLAIMS)
                 .setTimeToLiveSeconds(refreshTokenExpirationInSeconds)
-                .setMaxIdleSeconds(refreshTokenExpirationInSeconds));
+                .setMaxIdleSeconds(refreshTokenExpirationInSeconds)
+                .addEntryListenerConfig(listener));
+
         LOG.debug("hazelcast config={}", config);
         return config;
-        //                .setListenerConfigs(Arrays.asList(new ListenerConfig()
-        //                .setClassName(EntryChangeListener.class.getName())));
 
     }
 
