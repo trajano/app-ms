ee02605824feddaf54dc1c9c13fc2e90fa5a52fa
Archimedes Trajano <arch@trajano.net>
2017-12-22 09:59:20 -0500

    Authorization should be allowed to pass if Basic

--- ms-gateway/src/main/java/net/trajano/ms/gateway/internal/Predicates.java
+++ ms-gateway/src/main/java/net/trajano/ms/gateway/internal/Predicates.java
@@ -12,10 +12,22 @@ import java.util.stream.Stream;
 
 public final class Predicates {
 
+    /**
+     * A header is can be forwarded as long as it is not restricted.
+     * {@link io.vertx.core.http.HttpHeaders#AUTHORIZATION} is restricted unless it
+     * is "Basic" which implies that it is a client authorization that can be passed
+     * down.
+     */
     public static final Predicate<Map.Entry<String, String>> HEADER_FORWARDABLE;
 
+    /**
+     * Set of restricted headers.
+     */
     private static final Set<String> RESTRICTED_HEADERS;
 
+    /**
+     * URI that points to the JWKS.
+     */
     private static final String X_JWKS_URI = "X-JWKS-URI";
 
     private static final String X_JWT_ASSERTION = "X-JWT-Assertion";
@@ -23,10 +35,13 @@ public final class Predicates {
     private static final String X_JWT_AUDIENCE = "X-JWT-Audience";
 
     static {
-        RESTRICTED_HEADERS = Stream.of(X_JWKS_URI, X_JWT_ASSERTION, X_JWT_AUDIENCE, REQUEST_ID, AUTHORIZATION, DATE).map(CharSequence::toString).collect(Collectors.toSet());
-        HEADER_FORWARDABLE = e -> !RESTRICTED_HEADERS.contains(e.getKey());
+        RESTRICTED_HEADERS = Stream.of(X_JWKS_URI, X_JWT_ASSERTION, X_JWT_AUDIENCE, REQUEST_ID, DATE, AUTHORIZATION).map(CharSequence::toString).collect(Collectors.toSet());
+        HEADER_FORWARDABLE = e -> e.getKey().contentEquals(AUTHORIZATION) && e.getValue().startsWith("Basic ") || !RESTRICTED_HEADERS.contains(e.getKey());
     }
 
+    /**
+     * Prevent instantiation of utility class.
+     */
     private Predicates() {
 
     }
--- ms-gateway/src/test/java/net/trajano/ms/gateway/test/HeaderForwardablePredicateTest.java
+++ ms-gateway/src/test/java/net/trajano/ms/gateway/test/HeaderForwardablePredicateTest.java
@@ -2,8 +2,10 @@ package net.trajano.ms.gateway.test;
 
 import static junit.framework.TestCase.assertTrue;
 import static net.trajano.ms.gateway.providers.RequestIDProvider.REQUEST_ID;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -15,6 +17,9 @@ import net.trajano.ms.gateway.internal.Predicates;
 
 public class HeaderForwardablePredicateTest {
 
+    /**
+     * Tests the predicate.
+     */
     @Test
     public void testPredicate() {
 
@@ -29,6 +34,23 @@ public class HeaderForwardablePredicateTest {
         assertTrue(Predicates.HEADER_FORWARDABLE.test(x.next()));
     }
 
+    /**
+     * Tests that bearer authorization is filtered.
+     */
+    @Test
+    public void testRestrictedAuthorization() {
+
+        final Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Bearer abc");
+        headers.put("X-JWT-Assertion", "asdf");
+        headers.put("Date", "asdf");
+        headers.put("Allowed", "asdf");
+        final Map<String, String> y = headers.entrySet().stream().filter(Predicates.HEADER_FORWARDABLE).collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
+        assertTrue(y.containsKey("Allowed"));
+        assertEquals("Map should only have one entry " + y, 1, y.size());
+
+    }
+
     @Test
     public void testStream() {
 
@@ -38,7 +60,25 @@ public class HeaderForwardablePredicateTest {
         map.put("bar", "XXX");
         final Map<String, String> y = map.entrySet().stream().filter(Predicates.HEADER_FORWARDABLE).collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
         assertTrue(y.containsKey("foo"));
-        assertTrue(y.containsKey("foo"));
+        assertTrue(y.containsKey("bar"));
         assertFalse(y.containsKey(REQUEST_ID));
+    }
+
+    /**
+     * Tests that basic authorization is not filtered.
+     */
+    @Test
+    public void testUnrestrictedAuthorization() {
+
+        final Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Basic abc");
+        headers.put("X-JWT-Assertion", "asdf");
+        headers.put("Date", "asdf");
+        headers.put("Allowed", "asdf");
+        final Map<String, String> y = headers.entrySet().stream().filter(Predicates.HEADER_FORWARDABLE).collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
+        assertTrue(y.containsKey("Allowed"));
+        assertTrue(y.containsKey("Authorization"));
+        assertEquals("Map should only have two entries" + y, 2, y.size());
+
     }
 }
