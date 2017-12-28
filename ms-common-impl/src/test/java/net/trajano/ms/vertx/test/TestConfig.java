fa66dfd1db016f8f7e851ce0325a57b99f7892e7
Archimedes Trajano <developer@trajano.net>
2017-12-28 08:05:54 -0500

    (chore) Choose random port for server test

--- ms-common-impl/src/test/java/net/trajano/ms/vertx/test/TestConfig.java
+++ ms-common-impl/src/test/java/net/trajano/ms/vertx/test/TestConfig.java
@@ -1,12 +1,28 @@
 package net.trajano.ms.vertx.test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
+
+import io.vertx.core.http.HttpServerOptions;
 
 @Configuration
+
 public class TestConfig {
+
+    @Bean
+    public URI baseUri(@Qualifier("server.port") final int httpPort) {
+
+        return URI.create("http://localhost:" + httpPort);
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
@@ -14,5 +30,22 @@ public class TestConfig {
         final ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JaxbAnnotationModule());
         return objectMapper;
+    }
+
+    @Bean
+    @Primary
+    public HttpServerOptions serverOptions(@Qualifier("server.port") final int httpPort) {
+
+        return new HttpServerOptions()
+            .setPort(httpPort);
+    }
+
+    @Bean
+    @Qualifier("server.port")
+    public int serverPort() throws IOException {
+
+        try (final ServerSocket s = new ServerSocket(0)) {
+            return s.getLocalPort();
+        }
     }
 }
